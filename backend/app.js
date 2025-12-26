a74c9e6407feea7750142d0a363134e1c6d4df34
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2025-12-26 07:38:46 +0530

    Set up server with environment variables and middleware

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,19 @@
 import express from "express";
-
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 export const app = express();
+
+dotenv.config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
