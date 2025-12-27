d40f5d3f523c848dcc2b0ea41fc814d530f3a535
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2025-12-27 18:45:18 +0530

    Add user model with schema definition and password hashing

---
+++ backend/models/user.model.js
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please enter username"],
+      trim: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: [true, "Please enter fullName"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter email"],
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["Admin", "User"],
+      default: "User",
+    },
+    accountVerified: {
+      type: Boolean,
+      default: false,
+    },
+    borrowedBooks: [
+      {
+        bookId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Book",
+        },
+        returned: {
+          type: Boolean,
+          default: false,
+        },
+        borrowedDate: Date,
+        dueDate: Date,
+      },
+    ],
+
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+    refreshTokenExpire: Date,
+    verificationCode: Number,
+    verificationCodeExpire: Date,
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+  },
+  { timestamps: true }
+);
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+export const User = mongoose.model("User", userSchema);
