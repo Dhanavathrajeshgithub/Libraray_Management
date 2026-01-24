8ec9b59da1db9adef7733a1946da6c59ee8e61b1
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2026-01-24 18:02:07 +0530

    Add OTP verification actions and reducer to authSlice

--- client/src/store/slices/authSlice.js
+++ client/src/store/slices/authSlice.js
@@ -25,8 +25,46 @@ const authSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+
+    otpVerificationRequest(state) {
+      state.loading = true;
+      state.error = null;
+      state.message = null;
+    },
+    otpVerificationSuccess(state, action) {
+      state.loading = false;
+      state.message = action.payload.message;
+      state.isAuthenticated = true;
+      state.user = action.payload.user;
+    },
+    otpVerificationFailed(state, action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
+export const otpVerification = (email, otp) => async (dispatch) => {
+  dispatch(authSlice.actions.otpVerificationRequest());
+  await axios
+    .post(
+      "",
+      { email, otp },
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    )
+    .then((res) => {
+      dispatch(authSlice.actions.otpVerificationSuccess(res.data));
+    })
+    .catch((error) => {
+      dispatch(
+        authSlice.actions.otpVerificationFailed(error.response.data.message),
+      );
+    });
+};
 
 export const register = (data) => async (dispatch) => {
   dispatch(authSlice.actions.registerRequest());
