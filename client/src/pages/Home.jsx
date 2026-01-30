ea3e5a6e8ed4f49ef977bb6d3e696f0bb342639a
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2026-01-30 22:45:03 +0530

    Refactor SideBar component to remove unused code and integrate with Home component for sidebar toggle functionality

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,11 +13,18 @@ const Home = () => {
   }
   return (
     <>
-      <div>
-        <div>
-          <GiHamburgerMenu />
+      <div className="relative md:pl-64 flex min-h-screen bg-gray-100 ">
+        <div className="md:hidden z-10 absolute right-6 top-4 sm:top-6 flex justify-center items-center bg-black rounded-md h-9 w-9 text-white">
+          <GiHamburgerMenu
+            className="text-2xl"
+            onClick={() => setIsSideBarOpen(!isSideBarOpen)}
+          />
         </div>
-        <SideBar />
+        <SideBar
+          isSideBarOpen={isSideBarOpen}
+          setIsSideBarOpen={setIsSideBarOpen}
+          setSelectedComponent={setSelectedComponent}
+        />
       </div>
     </>
   );
