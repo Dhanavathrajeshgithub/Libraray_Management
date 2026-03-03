780a075be74a4b031207bc67e59a587ed625a02a
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2026-03-03 20:00:30 +0530

    Implement Forgot Password functionality; add email input handling, error notifications, and navigation for authenticated users

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/black-logo.png";
 import logo_with_title from "../assets/logo-with-title.png";
+import { useDispatch, useSelector } from "react-redux";
+import { forgotPassword, resetAuthSlice } from "../store/slices/authSlice";
+import { toast } from "react-toastify";
+import { Navigate } from "react-router-dom";
 
 const ForgotPassword = () => {
-  return <></>;
+  const [email, setEmail] = useState("");
+  const dispatch = useDispatch();
+  const { loading, error, message, user, isAuthenticated } = useSelector(
+    (state) => state.auth,
+  );
+  const handleForgotPassword = (e) => {
+    e.preventDefault();
+    dispatch(forgotPassword(email));
+  };
+
+  useEffect(() => {
+    // if (message) {
+    //   toast.success(message);
+    //   dispatch(resetAuthSlice());
+    // }
+    if (error) {
+      toast.error(error);
+      dispatch(resetAuthSlice());
+    }
+  }, [message, error, email, dispatch]);
+
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+  return (
+    <>
+      <div className="flex flex-col justify-center md:flex-row h-screen">
+        {/* LEFT SECTION */}
+        <div className="hidden w-full md:w-1/2 bg-black text-white md:flex flex-col items-center justify-center p-8 rounded-tr-[80px] rounded-br-[80px]">
+          <div className="text-center h-[450px]">
+            <div className="flex justify-center mb-12">
+              <img
+                src={logo_with_title}
+                alt="logo"
+                className="mb-12 h-44 w-auto"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* RIGHT SECTION */}
+        <div></div>
+      </div>
+    </>
+  );
 };
 
 export default ForgotPassword;
