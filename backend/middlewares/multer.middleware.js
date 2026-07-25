7bab539693b334f647370995083634b1c87bbb66
Dhanavath Rajesh <dhanavathrajesh@users.noreply.github.com>
2026-07-25 22:43:29 +0530

    changed multer code

--- backend/middlewares/multer.middleware.js
+++ backend/middlewares/multer.middleware.js
@@ -1,8 +1,10 @@
 import multer from "multer";
+import os from "os"; // 1. Import Node's built-in OS module
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    // 2. Change destination to the system's temporary directory
+    cb(null, os.tmpdir());
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
