1ac13eba9ae5c90a8ab9e4854f02804c28e0fe6b
Dhanavath Rajesh <dhanavathrajesh@users.noreply.github.com>
2026-07-26 15:14:59 +0530

    fix: removing service line from senEmail

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,7 +3,7 @@ import nodeMailer from "nodemailer";
 export const sendEmail = async ({ email, subject, message }) => {
   const transporter = nodeMailer.createTransport({
     host: process.env.SMTP_HOST,
-    service: process.env.SMTP_SERVICE,
+    // service: process.env.SMTP_SERVICE,
     port: process.env.SMTP_PORT,
     secure: false,
     auth: {
